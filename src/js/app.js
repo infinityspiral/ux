4e22290f29988f99c09d74f0658f158dce5bbbd2
Chris Norton <cnorton@aquent.com>
2020-04-21 10:59:54 -0500

    chore: update linting and autofix

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,63 @@
 window.onload = () => {
+  const videoEl = document.querySelectorAll('video')
 
-    const videoEl = document.querySelectorAll('video');
+  videoEl.forEach(item => {
+    item.defaultMuted = true
+    item.muted = true
+    item.loop = true
+    item.autoplay = true
+    item.addEventListener('click', e => {
+      console.log(item.paused)
+      e.currentTarget.play()
+    })
+    item.click()
+  })
 
-    videoEl.forEach(item=>{
+  const projectMenuBtn = document.querySelector('.expertise')
+  const quickNav = document.querySelector('.quick-nav')
+  const toggleProjectMenu = (e) => {
+    e.preventDefault()
+    quickNav.classList.toggle('active')
+    projectMenuBtn.classList.toggle('active')
+  }
 
-        item.defaultMuted = true;
-        item.muted = true;
-        item.loop = true;
-        item.autoplay = true;
-        item.addEventListener('click', e =>{
-            console.log(item.paused)
-            e.currentTarget.play();
-        });
-        item.click();
+  projectMenuBtn.addEventListener('click', e => {
+    toggleProjectMenu(e)
+  })
+
+  projectMenuBtn.addEventListener('touchstart', e => {
+    toggleProjectMenu(e)
+  })
+
+  const quickNavLinks = document.querySelectorAll('.project-jump')
+  quickNavLinks.forEach(item => {
+    item.addEventListener('click', e => {
+      e.preventDefault()
+
+      const dest = e.currentTarget.getAttribute('href')
+      const destEl = document.querySelector(dest.toString())
+      getCoords(destEl, -76)
+      quickNav.classList.remove('active')
+      projectMenuBtn.classList.remove('active')
     })
 
-    const projectMenuBtn = document.querySelector('.expertise');
-    const quickNav = document.querySelector('.quick-nav');
-    const toggleProjectMenu = (e) => {
-        e.preventDefault();
-        quickNav.classList.toggle("active");
-        projectMenuBtn.classList.toggle("active")
+    item.addEventListener('touchstart', e => {
+      e.preventDefault()
 
-    }
-
-    projectMenuBtn.addEventListener('click', e => {
-        toggleProjectMenu(e);
-    });
-
-    projectMenuBtn.addEventListener('touchstart', e => {
-        toggleProjectMenu(e);
-    });
-
-    const quickNavLinks = document.querySelectorAll('.project-jump');
-    quickNavLinks.forEach(item => {
-
-        item.addEventListener('click', e => {
-            e.preventDefault();
-
-            const dest = e.currentTarget.getAttribute('href');
-            const destEl = document.querySelector(dest.toString());
-            getCoords(destEl,-76);
-            quickNav.classList.remove("active");
-            projectMenuBtn.classList.remove("active")
-        })
-
-        item.addEventListener('touchstart', e => {
-            e.preventDefault();
-
-            const dest = e.currentTarget.getAttribute('href');
-            const destEl = document.querySelector(dest.toString());
-            getCoords(destEl,-116);
-            quickNav.classList.remove("active");
-            projectMenuBtn.classList.remove("active")
-        })
+      const dest = e.currentTarget.getAttribute('href')
+      const destEl = document.querySelector(dest.toString())
+      getCoords(destEl, -116)
+      quickNav.classList.remove('active')
+      projectMenuBtn.classList.remove('active')
     })
+  })
 
-    const getCoords = (elem,offset) => {
-        const scrollOffset = offset;
-        let coordsY = elem.getBoundingClientRect().top;
-        window.scrollBy({
-            top:coordsY+scrollOffset,
-            behavior:'auto' //or smooth
-        });
-    }
-
-};
+  const getCoords = (elem, offset) => {
+    const scrollOffset = offset
+    const coordsY = elem.getBoundingClientRect().top
+    window.scrollBy({
+      top: coordsY + scrollOffset,
+      behavior: 'auto' // or smooth
+    })
+  }
+}
